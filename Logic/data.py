92a403c7f5a0986bc8d8a622c4ad7a088b23ae2c
petarspal <petar.spalakarchev@yahoo.com>
2024-03-26 19:44:48 +0200

    Working on base functionality

--- Logic/data.py
+++ Logic/data.py
@@ -1,116 +1,134 @@
 import numpy as np
+from itertools import product
+
 
 def set_center(
-        value: int, data: np.ndarray, tendency: int = 0) -> None:
+        value: int, data: np.ndarray) -> None:
     '''
     Sets the center-most element of ndarray to value
     Parameters:
         value - value to set the element to
         data - ndarray
-        tendency - defines method of obtaining center
-            default 0 -> floor divide shape by 2
-            1 -> ceil divide shape by 2
-            2 -> alternate above two methods starting with floor
-            3 -> alternate above two methods starting with ceil
     '''
-    
-    def bottom_right(x):
+    def floor_round(x):
         return x // 2
-    
-    def top_left(x):
-        res, rem = divmod(x, 2)
-        return res-(rem ^ 1)
-    
-    bb = False
-    
-    def bottom_left(x):
-        nonlocal bb
-        bb = not bb
-        if bb:
-            return bottom_right(x)
-        return top_left(x)
-    
-    def top_right(x):
-        nonlocal bb
-        bb = not bb
-        return bottom_left(x)
-    
-    match tendency:
-        case 1:
-            tend = top_left
-        case 2:
-            tend = bottom_left
-        case 3:
-            tend = top_right
-        case _:
-            tend = bottom_right
-    
-    center_coords = map(tend, data.shape)
+    #In even-sized matrices, take the floor medians for center
+    #To get all possible centers we'll need all combinations of all floor and ceil medians across all dimensions
+    #Not worth
+    # def ceil_round(x):
+    #     res, rem = divmod(x, 2)
+    #     return res-(rem ^ 1)
+    center_coords = map(floor_round, data.shape)
     data[*center_coords] = value
-    
+
+
 def uniform_fill(value: int, data: np.ndarray) -> None:
     '''
     Fills the ndarray uniformly with the provided value
     '''
     data.fill(value)
-    
-def random_fill(value: int, data: np.ndarray):
+
+
+def random_fill(value: int, data: np.ndarray) -> None:
     '''
     Fills the ndarray with random ints in the range from 0 to the value
     '''
     data = np.random.randint(value, size=data.shape)
 
-def calc_begin_end(self, neighbor_count, left):
-    #nc-1 because there's always a center pixel
-    #div by 2 to check if the LR pixels are symmetrical
-    offset, even = divmod((neighbor_count-1), 2)
-    left = 1 if even and left else 0
-    right = 1 if even and not left else 0
-    #range of pixels in 1D
-    return(-offset-left, offset+1+right)
 
-def _calc_base_moore_idx(self, neighbor_count, left=True):
+def moore_one_d_ncount(totalncount: int, dimensions: int) -> int:
+    '''
+    Takes total neighbor count and dimensions as parameters
+    For the purpouses of Moore neighborhood shape
+    Returns an equivalent neighbor count in 1D (e.g. 1D= 3 -> 2d= 9 -> 3d= 27)
+    Throws exception is the ncount shape is not rationally reducible to 1D
+    (Aka if not Von Neuman or Moore -like shape)
+    '''
+    base = 3**(dimensions-1)
+    if not (1 == base):
+        one_d_ncount, rem = divmod(totalncount, base)
+        if not rem:
+            return one_d_ncount
+        raise Exception(f"invalid neighbor count for {base+1} dimensions")
+    return totalncount
+
+
+def moore_one_d_range_limits(
+        total_ncount: int, dimensions: int, tend_left: bool = True) -> tuple:
+    '''
+    Takes total neighbor count, dimensions as parameters.
+    Optionally takes direction to tend when not perfectly symmetrical
+        (default left)
     
-    def valid_ncount(number, base):
-        '''
-        Supposed to divide the ncount by the base
-        to validate the ncount is fully divisible
-        Is this curret that it's only done once?
-        '''
-        if not (1 == base):
-            ncount, rem = divmod(number, base)
-            if not rem:
-                return ncount
-            raise Exception(f"invalid neighbor count for {base+1} dimensions")
-        return number
+    For the purpouses of drawing a Moore neighbors shape
+    Generates the 1D line coordinates to be used as foundation of the shape
     
-    #3 to the power of dimensions-1 -- forgot
-    #I believe it had to do with there being 3 directions in each dimensions, center and one in each vector
-    #Aka when extruding to a new dimension we preserve the old dimension and then extrude in each vector in the new dim
-    #IDK
-    #1D automata -> 3**(1-1) = 1
-    #2D automata -> 3**(2-1) = 3
-    #3D automata -> 3**(3-1) = 9
-    #So here we're validating the count is divisible by the dimension multiplier?
-    neighbor_count = valid_ncount(neighbor_count, 3**(self.dimensions-1))
-    b,e = self._calc_begin_end(neighbor_count, left)
-    neighbor_range = range(b,e)
-    #obtaining the product of the range repeated in x dimensions thereby obtaining the range in all dim
-    neighbor_idxs = list(product(neighbor_range, repeat=self.dimensions))
+    Returns begin and end (+1) coordinates of the line
+    (end is +1 to account for usage in py range)
+    '''
+    one_d_ncount = moore_one_d_ncount(total_ncount, dimensions)
+    half_distance, even = divmod((one_d_ncount-1), 2)
+    #Handling even sized neighborhoods (asymmetric)
+    tendl, tendr = int(even and tend_left), int(even and (not tend_left))
+    begin, end = (-half_distance-tendl, half_distance+tendr+1)
+    return (begin, end)
+
+
+def calc_base_moore_idx(
+        neighbor_count: int, dimensions: int, left: bool = True) -> np.ndarray:
+    '''
+    Parameters:
+        neighbor_count - total neighbor count
+        dimensions - self explanatory
+        left - should automata lean left or right in even sized neighboohoods
+    Returns:
+        np.ndarray containing the indexes for a general moore neighboorhood
+        Center has 0 coordinates
+    '''
+    b, e = moore_one_d_range_limits(neighbor_count, dimensions, left)
+    neighbor_range = range(b, e)
+    neighbor_idxs = list(product(neighbor_range, repeat=dimensions))
     arr = np.array(neighbor_idxs)
-    ##offsetting by the coord
-    ##since all the work pertaining to the coord is done here
-    ##we can cache all previous steps
-    ##to avoid calculating for every coord
     return arr
-    
-def _get_moore_idx(self, coord, neighbor_count, left=True):
-    arr = self._calc_base_moore_idx(neighbor_count, left)
+
+
+def get_moore_idx(
+        coord: np.ndarray,
+        neighbor_count: int,
+        dimensions: int,
+        left: bool = True):
+    '''
+    Parameters:
+        coord - coordinate of center element
+        neighbor_count - total neighbor count
+        dimensions - self explanatory
+        left - should automata lean left or right in even sized neighboohoods
+    Returns:
+        np.ndarray of a moore neighborhood with the provider coord as center
+    '''
+    arr = calc_base_moore_idx(neighbor_count, dimensions, left)
     return arr + coord
-    
-def get_moore_neighbors(self, coord, neighbor_count, left=True):
-    idxarr = self._get_moore_idx(coord, neighbor_count, left)
-    return self.data[*idxarr.T]
+
+
+def get_moore_neighbors(
+        coord: np.ndarray,
+        data: np.ndarray,
+        neighbor_count: int,
+        dimensions: int,
+        left: bool = True):
+    '''
+    Parameters:
+        coord - coordinate of center element
+        data - data source
+        neighbor_count - total neighbor count
+        dimensions - self explanatory
+        left - should automata lean left or right in even sized neighboohoods
+    Returns:
+        np.ndarray with the daya of the moore neighbors
+    '''
+    #Is this needed as is?
+    idxarr = get_moore_idx(coord, neighbor_count, dimensions, left)
+    return data[*idxarr.T]
 
 # def mutate_moore_neighbors(self, coord, values, neighbor_count, left=True):
 # why mutate the actual neighbors??
@@ -143,7 +161,12 @@ def get_moore_neighbors(self, coord, neighbor_count, left=True):
 #         self.data[i] = raptor.io(self.data[*idxarr.T])
 
 
-g = np.zeros(tuple(4 for _ in range(2)))
-print(g)
 
-print(set_center(2, g))
+
+
+# g = np.zeros(tuple(4 for _ in range(2)))
+# print(g)
+
+# print(set_center(2, g))
+
+print(calc_begin_end(81,3))
